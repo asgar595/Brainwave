f32fee34f7189697886b9a308d87a6228164f7e0
ALI ASGAR <aliasgar.mithaiwala2022@vitstudent.ac.in>
2024-09-08 18:59:58 +0530

    navbar added

---
+++ Brainwave/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Navbar from './Components/Header/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+        <div className="gradient__bg">
+      <Navbar />
+     
+    </div>
+      
+    </div>
+  )
+}
+
+export default App
